783c5ea27fed37bfa9597a80870664f4fb9d57ef
michal-fixler <m0533131172@gmail.com>
2025-04-23 15:30:11 +0300

    Update services.php
    הוספתי את התוקן

--- services.php
+++ services.php
@@ -8,6 +8,14 @@ $functions = [ //
         'ajax'        => true,
         'services' => [
             MOODLE_OFFICIAL_MOBILE_SERVICE,
+    'local_groupmanager_generate_token' => [
+    'classname'   => 'local_groupmanager\external\generate_token',
+    'description' => 'Generates a token for a given username and password.',
+    'type'        => 'read',
+    'ajax'        => true,
+    'services'    => [MOODLE_OFFICIAL_MOBILE_SERVICE],
+],
+
         ]
     ],
 ];

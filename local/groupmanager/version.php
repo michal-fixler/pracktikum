7d576ee635f67294aa2efa5b58d3a9d20d165f46
michal-fixler <m0533131172@gmail.com>
2025-04-23 12:56:15 +0300

    Update version.php
    שיניתי גירסה

--- local/groupmanager/version.php
+++ local/groupmanager/version.php
@@ -3,7 +3,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_groupmanager';
-$plugin->version   = 2025042300;
+$plugin->version   = 2025042301;
 $plugin->requires  = 2022041900; // התאימי לגרסה של Moodle שלך
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '1.0';

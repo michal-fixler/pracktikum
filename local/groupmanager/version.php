014af378f0f0a557c83ed76301a1e55351bf1fec
michal-fixler <m0533131172@gmail.com>
2025-04-23 12:52:25 +0300

    Create version.php
    version

---
+++ local/groupmanager/version.php
@@ -0,0 +1,9 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_groupmanager';
+$plugin->version   = 2025042300;
+$plugin->requires  = 2022041900; // התאימי לגרסה של Moodle שלך
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release   = '1.0';
